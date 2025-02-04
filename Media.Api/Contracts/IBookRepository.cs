f74fc6a27dbb99d4bb8e2920a7d1f912e5542e50
Christer Carlsson <ccarlsson@outlook.com>
2025-02-04 15:18:01 +0100

    Add project files.

---
+++ Media.Api/Contracts/IBookRepository.cs
@@ -0,0 +1,12 @@
+﻿using Media.Api.Models;
+
+namespace Media.Api.Contracts;
+
+public interface IBookRepository
+{
+    Task<IEnumerable<Book>> GetBooksAsync();
+    Task<Book?> GetBookAsync(int id);
+    Task<Book> AddBookAsync(Book book);
+    Task<bool> UpdateBookAsync(int id, Book book);
+    Task<bool> DeleteBookAsync(int id);
+}
---
+++ Media.Api/Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace Media.Api.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public int Pages { get; set; }
+}
