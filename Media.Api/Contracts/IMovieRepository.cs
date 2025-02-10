af0cb0afa3f242f366647dfca4fcab0690c94e7c
Christer Carlsson <ccarlsson@outlook.com>
2025-02-10 09:29:58 +0100

    Add Movie repository

---
+++ Media.Api/Contracts/IMovieRepository.cs
@@ -0,0 +1,12 @@
+﻿using Media.Api.Models;
+
+namespace Media.Api.Contracts;
+
+public interface IMovieRepository
+{
+  Task<IEnumerable<Movie>> GetMoviesAsync();
+  Task<Movie?> GetMovieAsync(int id);
+  Task<Movie> AddMovieAsync(Movie movie);
+  Task<bool> UpdateMovieAsync(int id, Movie movie);
+  Task<bool> DeleteMovieAsync(int id);
+}
